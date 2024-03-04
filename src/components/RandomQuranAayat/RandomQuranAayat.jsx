94d7d7d39f7d96c9f36e098034677bd985c789aa
Enayet <enayetflweb@gamil.com>
2024-03-04 19:11:14 +0600

    scroll lock implemented

--- src/components/RandomQuranAayat/RandomQuranAayat.jsx
+++ src/components/RandomQuranAayat/RandomQuranAayat.jsx
@@ -1,4 +1,5 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
+import { disablePageScroll, enablePageScroll } from "scroll-lock";
 
 const RandomQuranAayat = () => {
   const [quran, setQuran] = useState(null);
@@ -6,9 +7,7 @@ const RandomQuranAayat = () => {
   const [modalOpen, setModalOpen] = useState(false);
 
   const getQuran = () => {
-    fetch(
-      'quran.json'
-    )
+    fetch("quran.json")
       .then((res) => res.json())
       .then((data) => {
         setQuran(data.quran);
@@ -36,22 +35,31 @@ const RandomQuranAayat = () => {
     return () => clearInterval(interval); // Cleanup interval
   }, [quran]);
 
+  useEffect(() => {
+    if (modalOpen) {
+      disablePageScroll();
+    } else {
+      enablePageScroll();
+    }
+  }, [modalOpen]);
+
+  const handleModalClose = () => {
+    setModalOpen(false);
+  };
+
   return (
     <div className=" flex flex-col justify-center items-center ">
       <div className="container">
-        {/* <h1 className="text-2xl font-bold">My other work</h1> */}
         <p></p>
       </div>
       {modalOpen && (
         <div className="fixed inset-0 w-full h-full flex justify-center items-center bg-black bg-opacity-50">
           <div className="bg-white p-8 rounded shadow-lg max-w-[600px]">
-            <h1 className=" font-bold mb-4  whitespace-normal">
-              {ayat?.text}
-            </h1>
+            <h1 className=" font-bold mb-4  whitespace-normal">{ayat?.text}</h1>
             <p className="text-gray-700">{`${ayat?.chapter} : ${ayat?.verse}`}</p>
             <button
-              onClick={() => setModalOpen(false)}
-              className="mt-4 bg-theme hover:bg-emerald-500 hover:text-white text-white font-bold py-2 px-6 rounded duration-300"
+              onClick={handleModalClose}
+              className="mt-4 bg-theme hover:bg-[#f1c40f] hover:text-white text-white font-bold py-2 px-6 rounded duration-300"
             >
               Close
             </button>
